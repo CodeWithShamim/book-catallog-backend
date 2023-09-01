64dcd30969b61e42ca0cd68e6356b1e0ea25c613
codewithshamim <shamimislamonline@gmail.com>
2023-09-01 11:54:46 +0600

    added prisma setup

---
+++ prisma/migrations/20230901055104_user/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "Roles" AS ENUM ('ADMIN', 'CUSTOMER');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "Roles" NOT NULL DEFAULT 'CUSTOMER',
+    "contactNo" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "profileImg" TEXT NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
